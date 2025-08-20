9c9fbf0cdf4c3c417cd802d6c68abe10f13e38ef
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-21 00:32:34 +0530

    Repository added

--- src/main/java/com/courses/repository/CourseRepository.java
+++ src/main/java/com/courses/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package com.courses.repository;
 import com.courses.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Repository added
 public interface CourseRepository extends JpaRepository<Course,Integer>
 {
 }
